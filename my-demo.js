9fd745cea746bd2dee331c647b927dfc0b36a9ed
kolinkuang <kolinkuang@github.com>
2020-08-02 22:59:51 +0800

    First commit

---
+++ my-demo.js
@@ -0,0 +1,375 @@
+/*
+*                  LISP                      C
+*
+*   2 + 2          (add 2 2)                 add(2, 2)
+*   4 - 2          (subtract 4 2)            subtract(4, 2)
+*   2 + (4 - 2)    (add 2 (subtract 4 2))    add(2, subtract(4, 2))
+*/
+
+// Parsing: Taking raw code and turning it into a more abstract representation of the code.
+    // Lexical Analysis to token by tokenizer
+    // Syntactic Analysis to Abstract Syntax Tree
+// Transformation: Takes this abstract representation and manipulates to do whatever the compiler wants it to.
+// Code Generation: Takes the transformed representation of the code and turns it into new code.
+
+//Tokens:
+/*
+[
+ { type: 'paren',  value: '('        },
+ { type: 'name',   value: 'add'      },
+ { type: 'number', value: '2'        },
+ { type: 'paren',  value: '('        },
+ { type: 'name',   value: 'subtract' },
+ { type: 'number', value: '4'        },
+ { type: 'number', value: '2'        },
+ { type: 'paren',  value: ')'        },
+ { type: 'paren',  value: ')'        },
+]
+*/
+
+// AST:
+/*
+{
+     type: 'Program',
+     body: [{
+       type: 'CallExpression',
+       name: 'add',
+       params: [{
+         type: 'NumberLiteral',
+         value: '2',
+       }, {
+         type: 'CallExpression',
+         name: 'subtract',
+         params: [{
+           type: 'NumberLiteral',
+           value: '4',
+         }, {
+           type: 'NumberLiteral',
+           value: '2',
+         }]
+       }]
+     }]
+}
+*/
+
+//Visitors:
+/*
+var visitor = {
+    NumberLiteral: {
+        enter(node, parent) {},
+        exit(node, parent) {}
+    },
+    CallExpression: {
+        enter(node, parent) {},
+        exit(node, parent) {}
+    }
+};
+*/
+
+/*
+* * ----------------------------------------------------------------------------
+*   Original AST                     |   Transformed AST
+* ----------------------------------------------------------------------------
+*   {                                |   {
+*     type: 'Program',               |     type: 'Program',
+*     body: [{                       |     body: [{
+*       type: 'CallExpression',      |       type: 'ExpressionStatement',
+*       name: 'add',                 |       expression: {
+*       params: [{                   |         type: 'CallExpression',
+*         type: 'NumberLiteral',     |         callee: {
+*         value: '2'                 |           type: 'Identifier',
+*       }, {                         |           name: 'add'
+*         type: 'CallExpression',    |         },
+*         name: 'subtract',          |         arguments: [{
+*         params: [{                 |           type: 'NumberLiteral',
+*           type: 'NumberLiteral',   |           value: '2'
+*           value: '4'               |         }, {
+*         }, {                       |           type: 'CallExpression',
+*           type: 'NumberLiteral',   |           callee: {
+*           value: '2'               |             type: 'Identifier',
+*         }]                         |             name: 'subtract'
+*       }]                           |           },
+*     }]                             |           arguments: [{
+*   }                                |             type: 'NumberLiteral',
+*                                    |             value: '4'
+* ---------------------------------- |           }, {
+*                                    |             type: 'NumberLiteral',
+*                                    |             value: '2'
+*                                    |           }]
+*  (sorry the other one is longer.)  |         }
+*                                    |       }
+*                                    |     }]
+*                                    |   }
+* ----------------------------------------------------------------------------
+* */
+
+const WHITESPACE = /\s/;
+const NUMBERS = /[0-9]/;
+const LETTERS = /[a-z]/i;   // case insensitive
+
+function tokenizer(input) {
+    let current = 0;
+    let tokens = [];
+
+    while (current < input.length) {
+        let char = input[current];
+        if (char === '(') {
+            tokens.push({
+                type: 'paren',
+                value: char
+            });
+
+            current++;
+            continue;
+        }
+
+        if (char === ')') {
+            tokens.push({
+                type: 'paren',
+                value: char
+            });
+
+            current++;
+            continue;
+        }
+
+        if (WHITESPACE.test(char)) {
+            current++;
+            continue;
+        }
+
+        if (NUMBERS.test(char)) {
+            let value = '';
+            while (NUMBERS.test(char)) {
+                value += char;
+                char = input[++current];
+            }
+            tokens.push({
+                type: 'number',
+                value
+            });
+            continue;
+        }
+
+        if (char === '"') {
+            let value = '';
+            char  = input[++current];
+            while (char !== '"') {
+                value += char;
+                char = input[++current];
+            }
+            char = input[++current];
+            tokens.push({
+                type: 'string',
+                value
+            });
+            continue;
+        }
+
+        if (LETTERS.test(char)) {
+            let value = '';
+            while (LETTERS.test(char)) {
+                value += char;
+                char = input[++current];
+            }
+            tokens.push({
+                type: 'name',
+                value
+            });
+            continue;
+        }
+
+        throw new TypeError('I dont know what this character is: ' + char);
+    }
+
+    return tokens;
+}
+
+function parser(tokens) {
+    // 回溯递归
+    let current = 0;
+
+    function walk() {
+        let token = tokens[current];
+
+        if (token.type === 'number') {
+            current++;
+            return {
+                type: 'NumberLiteral',
+                value: token.value
+            };
+        }
+
+        if (token.type === 'string') {
+            current++;
+            return {
+                type: 'StringLiteral',
+                value: token.value
+            };
+        }
+
+        if (token.type === 'paren' && token.value === '(') {
+            token = tokens[++current];
+            let node = {
+                type: 'CallExpression',
+                name: token.value,
+                params: []
+            };
+
+            token = tokens[++current];
+
+            while (token.type !== 'paren' || token.value !== ')') {
+                node.params.push(walk());
+                token = tokens[current];
+            }
+
+            current++;
+
+            return node;
+        }
+
+        throw new TypeError(token.type);
+    }
+
+    let ast = {
+        type: 'Program',
+        body: []
+    };
+
+    while (current < tokens.length) {
+        ast.body.push(walk());
+    }
+
+    return ast;
+}
+
+function transformer(ast) {
+
+    // noinspection DuplicatedCode
+    function traverser(ast, visitor) {
+
+        function traverseArray(array, parent) {
+            array.forEach(child => {
+                traverseNode(child, parent);
+            });
+        }
+
+        function traverseNode(node, parent) {
+            let methods = visitor[node.type];
+
+            if (methods && methods.enter) {
+                methods.enter(node, parent);
+            }
+
+            switch(node.type) {
+                case 'Program':
+                    traverseArray(node.body, node);
+                    break;
+                case 'CallExpression':
+                    traverseArray(node.params, node);
+                    break;
+                case 'NumberLiteral':
+                case 'StringLiteral':
+                    break;
+                default:
+                    throw new TypeError(node.type);
+            }
+
+            if (methods && methods.exit) {
+                methods.exit(node, parent);
+            }
+        }
+
+        traverseNode(ast, null);
+    }
+
+    let newAst = {
+        type: 'Program',
+        body: []
+    };
+
+    ast._context = newAst.body;
+
+    traverser(ast, {
+        NumberLiteral: {
+            enter(node, parent) {
+                parent._context.push({
+                    type: 'NumberLiteral',
+                    value: node.value
+                });
+            }
+        },
+
+        StringLiteral: {
+            enter(node, parent) {
+                parent._context.push({
+                    type: 'StringLiteral',
+                    value: node.value
+                });
+            }
+        },
+
+        CallExpression: {
+            enter(node, parent) {
+                let expression = {
+                    type: 'CallExpression',
+                    callee: {
+                        type: 'Identifier',
+                        name: node.name
+                    },
+                    arguments: []
+                };
+
+                node._context = expression.arguments;
+
+                if (parent.type !== 'CallExpression') {
+                    expression = {
+                        type: 'ExpressionStatement',
+                        expression
+                    };
+                }
+
+                parent._context.push(expression);
+            }
+        }
+    });
+
+    return newAst;
+}
+
+function codeGenerator(node) {
+    switch(node.type) {
+        case 'Program':
+            return node.body.map(codeGenerator)
+                .join('\n');
+        case 'ExpressionStatement':
+            return codeGenerator(node.expression) + ';';
+        case 'CallExpression':
+            return codeGenerator(node.callee) + `(${node.arguments.map(codeGenerator).join(', ')})`;
+        case 'Identifier':
+            return node.name;
+        case 'NumberLiteral':
+            return node.value;
+        case 'StringLiteral':
+            return `"${node.value}"`;
+        default:
+            throw new TypeError(node.type);
+    }
+}
+
+function compiler(input) {
+    let tokens = tokenizer(input);
+    let ast = parser(tokens);
+    let newAst = transformer(ast);
+    let output = codeGenerator(newAst);
+
+    return output;
+}
+
+module.exports = {
+    tokenizer,
+    parser,
+    transformer,
+    codeGenerator,
+    compiler
+};
---
+++ my-demo2.js
@@ -0,0 +1,308 @@
+/* global module */
+
+const RegexPattern = {
+    Parentheses: '[()]',
+    WhiteSpace: '\\s',
+    Numbers: '[0-9]',
+    Letters: '[a-zA-Z]',
+    LeftParenthesis: '[(]'
+};
+
+/**
+Input LISP statement:
+(add 2 (subtract 4 2))
+
+Output Tokens:
+[
+ { type: 'paren',  value: '('        },
+ { type: 'name',   value: 'add'      },
+ { type: 'number', value: '2'        },
+ { type: 'paren',  value: '('        },
+ { type: 'name',   value: 'subtract' },
+ { type: 'number', value: '4'        },
+ { type: 'number', value: '2'        },
+ { type: 'paren',  value: ')'        },
+ { type: 'paren',  value: ')'        },
+]
+**/
+function _tokenizer(input) {
+
+    const NumbersRegExp = new RegExp(RegexPattern.Numbers);
+    const LettersRegExp = new RegExp(RegexPattern.Letters);
+
+    const TokenizerMap = {
+        [RegexPattern.WhiteSpace]: ({current, tokens}) => ({
+            current: ++current,
+            tokens
+        }),
+        [RegexPattern.Parentheses]: ({char, current, tokens}) => {
+            tokens.push({
+                type: 'paren',
+                value: char
+            });
+            return {
+                current: ++current,
+                tokens
+            };
+        },
+        [RegexPattern.Numbers]: ({char, current, tokens}) => {
+            let value = '';
+            while (NumbersRegExp.test(char)) {
+                value += char;
+                char = input[++current];
+            }
+            tokens.push({
+                type: 'number',
+                value
+            });
+            return {
+                current,
+                tokens
+            };
+        },
+        [RegexPattern.Letters]: ({char, current, tokens}) => {
+            let value = '';
+            while (LettersRegExp.test(char)) {
+                value += char;
+                char = input[++current];
+            }
+            tokens.push({
+                type: 'name',
+                value
+            });
+            return {
+                current,
+                tokens
+            };
+        }
+    };
+
+    let current = 0;
+    let tokens = [];
+
+    while (current < input.length) {
+        let char = input[current];
+        let foundPattern = Object.keys(TokenizerMap).find(pattern => new RegExp(pattern).test(char));
+        if (foundPattern) {
+            let result = TokenizerMap[foundPattern]({char, current, tokens});
+            current = result.current;
+            tokens = result.tokens;
+            continue;
+        }
+        throw new TypeError(`Unknown character: ${char}`);
+    }
+
+    return tokens;
+}
+
+/**
+ Input Tokens:
+ [
+ { type: 'paren',  value: '('        },
+ { type: 'name',   value: 'add'      },
+ { type: 'number', value: '2'        },
+ { type: 'paren',  value: '('        },
+ { type: 'name',   value: 'subtract' },
+ { type: 'number', value: '4'        },
+ { type: 'number', value: '2'        },
+ { type: 'paren',  value: ')'        },
+ { type: 'paren',  value: ')'        },
+ ]
+
+ Output AST:
+ {
+     type: 'Program',
+     body: [{
+       type: 'CallExpression',
+       name: 'add',
+       params: [{
+         type: 'NumberLiteral',
+         value: '2',
+       }, {
+         type: 'CallExpression',
+         name: 'subtract',
+         params: [{
+           type: 'NumberLiteral',
+           value: '4',
+         }, {
+           type: 'NumberLiteral',
+           value: '2',
+         }]
+       }]
+     }]
+}
+ **/
+function _parser(tokens) {
+    let current = 0;
+
+    const ParserMap = {
+        [RegexPattern.Numbers]: token => {
+            current++;
+            return {
+                type: 'NumberLiteral',
+                value: token.value
+            };
+        },
+
+        [RegexPattern.LeftParenthesis]: token => {
+            token = tokens[++current];
+            let node = {
+                type: 'CallExpression',
+                name: token.value,
+                params: []
+            };
+            token = tokens[++current];
+            while (token.value !== ')') {
+                // Used Backtrack
+                node.params.push(_walk());
+                token = tokens[current];
+            }
+            current++;
+            return node;
+        }
+    };
+
+    function _walk() {
+        let token = tokens[current];
+        let foundPattern = Object.keys(ParserMap).find(pattern => new RegExp(pattern).test(token.value + ''));
+        if (foundPattern) {
+            return ParserMap[foundPattern](token);
+        }
+        throw new TypeError(`Unknown token: ${token.type}`);
+    }
+
+    let ast = {
+        type: 'Program',
+        body: []
+    };
+
+    while(current < tokens.length) {
+        ast.body.push(_walk());
+    }
+
+    return ast;
+}
+
+/**
+ ----------------------------------------------------------------------------
+   Input AST                        |   Output AST
+ ----------------------------------------------------------------------------
+   {                                |   {
+     type: 'Program',               |     type: 'Program',
+     body: [{                       |     body: [{
+       type: 'CallExpression',      |       type: 'ExpressionStatement',
+       name: 'add',                 |       expression: {
+       params: [{                   |         type: 'CallExpression',
+         type: 'NumberLiteral',     |         callee: {
+         value: '2'                 |           type: 'Identifier',
+       }, {                         |           name: 'add'
+         type: 'CallExpression',    |         },
+         name: 'subtract',          |         arguments: [{
+         params: [{                 |           type: 'NumberLiteral',
+           type: 'NumberLiteral',   |           value: '2'
+           value: '4'               |         }, {
+         }, {                       |           type: 'CallExpression',
+           type: 'NumberLiteral',   |           callee: {
+           value: '2'               |             type: 'Identifier',
+         }]                         |             name: 'subtract'
+       }]                           |           },
+     }]                             |           arguments: [{
+   }                                |             type: 'NumberLiteral',
+                                    |             value: '4'
+ ---------------------------------- |           }, {
+                                    |             type: 'NumberLiteral',
+                                    |             value: '2'
+                                    |           }]
+  (sorry the other one is longer.)  |         }
+                                    |       }
+                                    |     }]
+                                    |   }
+ ----------------------------------------------------------------------------
+ **/
+function _transformer(ast) {
+
+    function _traverser(ast, visitor) {
+
+    }
+
+
+
+    return null;
+}
+
+/**
+ Input Transformed AST:
+{
+  type: 'Program',
+  body: [{
+    type: 'ExpressionStatement',
+    expression: {
+      type: 'CallExpression',
+      callee: {
+        type: 'Identifier',
+        name: 'add'
+      },
+      arguments: [{
+        type: 'NumberLiteral',
+        value: '2'
+      }, {
+        type: 'CallExpression',
+        callee: {
+          type: 'Identifier',
+          name: 'subtract'
+        },
+        arguments: [{
+          type: 'NumberLiteral',
+          value: '4'
+        }, {
+          type: 'NumberLiteral',
+          value: '2'
+        }]
+      }
+    }
+  }]
+}
+
+ Output C statement:
+ add(2, subtract(4, 2))
+ **/
+function _codeGenerator(newAst) {
+
+}
+
+/** Wrapped Functions **/
+function start(input) {
+    return tokenizerFn => tokenizerFn && tokenizerFn(input);
+}
+
+function tokenizer(input) {
+    let tokens = _tokenizer(input);
+    return parserFn => parserFn && parserFn(tokens);
+}
+
+function parser(tokens) {
+    let ast = _tokenizer(tokens);
+    return transformerFn => transformerFn && transformerFn(ast);
+}
+
+function transformer(ast) {
+    let newAst = _transformer(ast);
+    return codeGeneratorFn => codeGeneratorFn && codeGeneratorFn(newAst);
+}
+
+function codeGenerator(newAst) {
+    return () => _codeGenerator(newAst);
+}
+
+function compiler(input) {
+    // codeGenerator(transformer(parser(tokenizer())));
+    return start(input)(tokenizer)(parser)(transformer)(codeGenerator)();
+}
+
+module.exports = {
+    tokenizer: _tokenizer,
+    parser: _parser,
+    transformer: _transformer,
+    codeGenerator: _codeGenerator,
+    compiler
+};
